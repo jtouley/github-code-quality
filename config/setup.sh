2109a76058a1fed6b7894cc336fdb7d160ccc91f
jtouley <jtouley@gmail.com>
2025-02-17 14:30:04 -0500

    fix bug in code_quality.yml and added local test for adding comment to PR

--- config/setup.sh
+++ config/setup.sh
@@ -5,7 +5,7 @@ python3 -m venv venv
 source venv/bin/activate
 
 echo "Installing dependencies..."
-pip install -r requirements.txt
+pip install -r config/requirements.txt
 
 echo "Installing linting tools (black, flake8)..."
 pip install black flake8
